c6d90d934a9fa88f3a0c518be6b847318575c3f0
NAVYA242 <pendemnavya01@gmail.com>
2022-11-23 17:16:25 +0530

    learned about express

---
+++ data.js
@@ -0,0 +1,7 @@
+const students=[
+	{id:1,name:"Anu",class:"CAMS"},
+	{id:2,name:"Deepu",class:"MSCS"},
+	{id:3,name:"Ammu",class:"CAME"}
+]
+
+module.exports=students
---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const port=2000;
+const students=require("./data.js")
+
+const employee={id:11,name:"react",position:"frontend"}
+
+
+app.use(cors())
+app.get("/",(req,res)=>{
+	res.send("starting route")
+})
+app.get("/demo",(req,res)=>{
+	res.send("This is demo route")
+})
+app.get("/emp",(req,res)=>{
+	res.json(employee)
+})
+app.get("/students-list",(req,res)=>{
+	res.json(students)
+})
+
+app.listen(port,()=>console.log("server is running on port 2000"))
